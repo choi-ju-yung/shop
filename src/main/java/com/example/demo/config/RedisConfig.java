3e4426337e2408f92feba1b6b0885d2ce5fb4165
choijuyung <bogdudhkd123@naver.com>
2026-05-22 10:09:25 +0900

    Spring Session Redis 제거 - sticky session 방식으로 전환

--- src/main/java/com/example/demo/config/RedisConfig.java
+++ src/main/java/com/example/demo/config/RedisConfig.java
@@ -2,11 +2,6 @@
 
 	import java.time.Duration;
 
-	import com.fasterxml.jackson.annotation.JsonAutoDetect;
-	import com.fasterxml.jackson.annotation.JsonTypeInfo;
-	import com.fasterxml.jackson.annotation.PropertyAccessor;
-	import com.fasterxml.jackson.databind.ObjectMapper;
-	import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 	import org.springframework.cache.annotation.EnableCaching;
 	import org.springframework.context.annotation.Bean;
 	import org.springframework.context.annotation.Configuration;
@@ -15,14 +10,10 @@
 	import org.springframework.data.redis.connection.RedisConnectionFactory;
 	import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 	import org.springframework.data.redis.serializer.RedisSerializationContext;
-	import org.springframework.data.redis.serializer.RedisSerializer;
-	import org.springframework.security.jackson2.SecurityJackson2Modules;
-	import org.springframework.security.oauth2.client.jackson2.OAuth2ClientJackson2Module;
 
 	@Configuration
 	@EnableCaching
 	public class RedisConfig {
-
 		@Bean
 		public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
 			RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
@@ -32,19 +23,5 @@
 					.disableCachingNullValues();
 
 			return RedisCacheManager.builder(connectionFactory).cacheDefaults(config).build();
-		}
-
-		@Bean("springSessionDefaultRedisSerializer")
-		public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
-			ObjectMapper mapper = new ObjectMapper();
-			mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-			mapper.activateDefaultTyping(
-				LaissezFaireSubTypeValidator.instance,
-				ObjectMapper.DefaultTyping.NON_FINAL,
-				JsonTypeInfo.As.PROPERTY
-			);
-			mapper.registerModules(SecurityJackson2Modules.getModules(getClass().getClassLoader()));
-			mapper.registerModule(new OAuth2ClientJackson2Module());
-			return new GenericJackson2JsonRedisSerializer(mapper);
 		}
 	}
