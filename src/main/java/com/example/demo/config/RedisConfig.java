fdc172be0278d2469cfe732c9b3fe84e321a31ab
choijuyung <bogdudhkd123@naver.com>
2026-05-22 10:06:35 +0900

    Spring Security OAuth2 Jackson 모듈 등록 - 카카오 세션 직렬화 수정

--- src/main/java/com/example/demo/config/RedisConfig.java
+++ src/main/java/com/example/demo/config/RedisConfig.java
@@ -2,6 +2,11 @@
 
 	import java.time.Duration;
 
+	import com.fasterxml.jackson.annotation.JsonAutoDetect;
+	import com.fasterxml.jackson.annotation.JsonTypeInfo;
+	import com.fasterxml.jackson.annotation.PropertyAccessor;
+	import com.fasterxml.jackson.databind.ObjectMapper;
+	import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 	import org.springframework.cache.annotation.EnableCaching;
 	import org.springframework.context.annotation.Bean;
 	import org.springframework.context.annotation.Configuration;
@@ -11,10 +16,13 @@
 	import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 	import org.springframework.data.redis.serializer.RedisSerializationContext;
 	import org.springframework.data.redis.serializer.RedisSerializer;
+	import org.springframework.security.jackson2.SecurityJackson2Modules;
+	import org.springframework.security.oauth2.client.jackson2.OAuth2ClientJackson2Module;
 
 	@Configuration
 	@EnableCaching
 	public class RedisConfig {
+
 		@Bean
 		public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
 			RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
@@ -28,6 +36,15 @@
 
 		@Bean("springSessionDefaultRedisSerializer")
 		public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
-			return new GenericJackson2JsonRedisSerializer();
+			ObjectMapper mapper = new ObjectMapper();
+			mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+			mapper.activateDefaultTyping(
+				LaissezFaireSubTypeValidator.instance,
+				ObjectMapper.DefaultTyping.NON_FINAL,
+				JsonTypeInfo.As.PROPERTY
+			);
+			mapper.registerModules(SecurityJackson2Modules.getModules(getClass().getClassLoader()));
+			mapper.registerModule(new OAuth2ClientJackson2Module());
+			return new GenericJackson2JsonRedisSerializer(mapper);
 		}
 	}
